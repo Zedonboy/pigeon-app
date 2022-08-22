557826fd710a9c6cf4c29d18a8bd216779e377e0
Declan Nnadozie <zedonbiz@gmail.com>
2022-08-22 02:06:04 +0100

    update

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 export const PROPOSAL_APP_ID = 2313243
 export const PARTICIPATION_ASSET_ID = 105293033
 export const GITHUB_TOKEN = "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"
-export const API_URL = "http://localhost:1337/api"
+export const API_URL = process.env.NODE_ENV === "production" ? "https://pigeon-strapi.herokuapp.com/api" : "http://localhost:1337/api"
 export const DAO_DAPP_ID = 105302377 //TODO(Get proper App Id)
 export const DAO_ADDRESS = "G2CHFTKB5RZAWQLG2SZICUEI5JP7UGQ4743EI6U3IQIOPYGPOV3WXOM4NY"
 export const DAO_CREATOR_ADDRESS = ""
